6fcb21f98b0b64816345beb7846168c54d7bd54a
MatteoSottosanti <ma.sottosanti@gmail.com>
2020-12-04 16:53:27 +0100

    aggiunta userView

--- src/main/java/it/corsobackendtree/treebooking/controllers/TreeBookingController.java
+++ src/main/java/it/corsobackendtree/treebooking/controllers/TreeBookingController.java
@@ -1,7 +1,20 @@
 package it.corsobackendtree.treebooking.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import it.corsobackendtree.treebooking.views.UserView;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TreeBookingController {
+    @PostMapping("/user")
+    UserView signUpUser(@RequestBody UserView userToSignUp){
+
+        //cod 201
+        return null;
+    }
+
+/*
+    @GetMapping("/login")
+    UserView logIn(@RequestParam )
+*/
+
 }
