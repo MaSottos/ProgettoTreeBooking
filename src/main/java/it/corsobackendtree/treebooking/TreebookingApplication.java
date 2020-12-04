20a3300f2b13b45a9781b0e21ba2def17d23e890
MatteoSottosanti <ma.sottosanti@gmail.com>
2020-12-04 12:04:34 +0100

    primoCommit

---
+++ src/main/java/it/corsobackendtree/treebooking/TreebookingApplication.java
@@ -0,0 +1,13 @@
+package it.corsobackendtree.treebooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreebookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreebookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/corsobackendtree/treebooking/TreebookingApplicationTests.java
@@ -0,0 +1,13 @@
+package it.corsobackendtree.treebooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreebookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
